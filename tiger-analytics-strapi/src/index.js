738ea5380115dca5b70a8bac501a1a48291b47ff
Og-Akash <akashghoshofficial700@gmail.com>
2026-01-22 14:25:16 +0530

    modify kao into index file.

--- tiger-analytics-strapi/src/index.js
+++ tiger-analytics-strapi/src/index.js
@@ -1,17 +1,31 @@
 module.exports = {
   register({ strapi }) {
-    // Force trust proxy headers
-    strapi.server.proxy = true;
+    // Get the underlying Koa app
+    const app = strapi.server.app;
     
-    // Manually set secure flag based on X-Forwarded-Proto
-    strapi.server.use(async (ctx, next) => {
-      const proto = ctx.get('x-forwarded-proto');
-      if (proto === 'https') {
-        ctx.request.secure = true;
-        ctx.request.protocol = 'https';
-      }
-      await next();
-    });
+    // Force proxy trust
+    app.proxy = true;
+    
+    // Intercept ALL requests at the earliest possible point
+    const originalCallback = app.callback();
+    
+    app.callback = function() {
+      return async (req, res) => {
+        // Check the x-forwarded-proto header from the raw Node request
+        const proto = req.headers['x-forwarded-proto'];
+        
+        if (proto === 'https') {
+          // Modify the encrypted property which Koa uses
+          Object.defineProperty(req.socket, 'encrypted', {
+            value: true,
+            configurable: true,
+            writable: true
+          });
+        }
+        
+        return originalCallback(req, res);
+      };
+    };
   },
 
   bootstrap(/*{ strapi }*/) {},
